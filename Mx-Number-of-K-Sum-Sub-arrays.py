d7b8f0ae3a01b3604cb4283ec1e226055944e587
hannankm <115619624+hannankm@users.noreply.github.com>
2022-10-28 21:17:13 +0300

    Create Mx-Number-of-K-Sum-Sub-arrays.py

---
+++ Mx-Number-of-K-Sum-Sub-arrays.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        start=operation=0
+        end=len(nums)-1
+        while start<end :
+            if nums[start]+ nums[end]==k:
+                #delete elements
+                nums[start]=nums[end]==0
+                #increment operation
+                operation+=1
+               #increment star and end operators
+                start+=1
+                end-=1
+            elif nums[start]+nums[end]>k:
+                 #increment end pointer
+                end-=1
+            else:
+                #increment start pointer
+                start+=1    
+        return operation
